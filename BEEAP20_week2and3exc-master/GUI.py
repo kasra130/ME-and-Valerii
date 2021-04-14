8ac237bd82c533ff7f8694197ed617eb99cf6da3
Casper <casper.jarnefelt@gmail.com>
2021-04-14 19:42:37 +0300

    Progress on the dropdown
    Its an absolute mess but I did get the drop down to be empty and right now, it is even better, ,,,,, someting is there but it shows complete junk, because my code doesnt make sense.

--- BEEAP20_week2and3exc-master/GUI.py
+++ BEEAP20_week2and3exc-master/GUI.py
@@ -28,7 +28,7 @@ def windowsize():                                   #window size
 
 def windowtitle():                  #window title and icon
     root.title("casper and valeeri's program")     #title
-    root.iconbitmap(r'C:\Users\caspe\Desktop\Git test\ME-and-Volerii\BEEAP20_week2and3exc-master\image\favicon.ico')  #icon image 
+    root.iconbitmap(r'C:\Users\caspe\Desktop\Git test\ME-and-Volerii\BEEAP20_week2and3exc-master\images\favicon.ico')  #icon image 
 
     
 
@@ -40,7 +40,6 @@ def windowfeatures():    #screen (inside) color
 <<<<<<< Updated upstream
 def openfile():   #command
     return fd.askopenfilename()   # definig the open file command for the button 
-    filepath=fd.askopenfilename() #not good
 
 def Pbutton():       #button for open gile 
     Button(root, text="Open file...", command=openfile).place(x=30, y=70, width=80, height=25)  #resizing button, and giving it a position, text and action command 
@@ -85,9 +84,6 @@ windowsize()
 windowtitle()
 windowfeatures()
 Pbutton()
-
-file = open(openfile)
-print(file)
 
 
 
