5c67fc97ba13f3d1c0cfbea9a8db10d32c89bef2
Casper <casper.jarnefelt@gmail.com>
2021-04-15 17:43:07 +0300

    thisone.py now is the main prog
    I hate git,

--- BEEAP20_week2and3exc-master/Thisone.py
+++ BEEAP20_week2and3exc-master/Thisone.py
@@ -17,8 +17,11 @@ class window:
         self.root.iconbitmap(icon)     #window icon
         self.root.configure(bg="black")#preset color to black coz we are goths  
         root.resizable(width=False, height=False)   #disabling resizing for consistant presentation 
+<<<<<<< Updated upstream:BEEAP20_week2and3exc-master/Thisone.py
 <<<<<<< Updated upstream:BEEAP20_week2and3exc-master/window.py
 <<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes:BEEAP20_week2and3exc-master/window.py
         self.heading=Label(self.root, text="Data analyzer", fg="white", bg="black",font=("Times",20)).grid(row=1, column=1)  #title on the screen
 
 
@@ -27,6 +30,7 @@ class window:
         self.exitbutton=Button(self.root, text="Exit", command=exit).place(x=3, y=660, width=60, height=25) #exit button
         self.helpbutton=Button(self.root, text="Help", command= lambda: ms.showinfo("Information",info)).place(x=830,y=10, width=60, height=25)
           
+<<<<<<< Updated upstream:BEEAP20_week2and3exc-master/Thisone.py
 =======
 =======
 >>>>>>> Stashed changes:BEEAP20_week2and3exc-master/Thisone.py
@@ -43,10 +47,13 @@ class window:
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes:BEEAP20_week2and3exc-master/Thisone.py
+=======
+>>>>>>> Stashed changes:BEEAP20_week2and3exc-master/window.py
 
 
 
         self.combo=ttk.Combobox(root)
+<<<<<<< Updated upstream:BEEAP20_week2and3exc-master/Thisone.py
 <<<<<<< Updated upstream:BEEAP20_week2and3exc-master/window.py
 <<<<<<< Updated upstream
         self.combo.place(x=350, y=90, width=300, height=30)
@@ -55,13 +62,14 @@ class window:
 =======
 =======
 >>>>>>> Stashed changes:BEEAP20_week2and3exc-master/Thisone.py
+=======
+>>>>>>> Stashed changes:BEEAP20_week2and3exc-master/window.py
         self.combo.place(x=250, y=90, width=300, height=30)
         self.combo.bind("<<comboboxSelected>>",combo )#t
 
 
         
         
->>>>>>> Stashed changes
 
         self.root.mainloop()
 
@@ -71,19 +79,9 @@ class window:
 
 
 
-<<<<<<< Updated upstream
-class Backend:
-    def __init__(self):
-        pass
-    @staticmethod
-    def openfile(self):
-        csv_file_path = fd.askopenfilename(title="select a CSV file",filetypes=(("csv files", "*.csv"),)) #only csv accepted
-        return csv_file_path
-=======
      
         
       
->>>>>>> Stashed changes
     
     def readingmethod(self):
         v=csv_file_path
@@ -98,8 +96,11 @@ label_file=Label(root,text="this").place(x=30, y=400, width=60, height=25)
 
  
 def screen():
+<<<<<<< Updated upstream:BEEAP20_week2and3exc-master/Thisone.py
 <<<<<<< Updated upstream:BEEAP20_week2and3exc-master/window.py
 <<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes:BEEAP20_week2and3exc-master/window.py
     window1=window(root,"casper and valeeri's program", "900x700", r'C:\Users\caspe\Desktop\Git test\ME-and-Volerii\BEEAP20_week2and3exc-master\image\favicon.ico',"black")
 
 def readnow():
@@ -112,35 +113,6 @@ screen()
 
 readnow()
 
-=======
-    window1=window(root,"casper and valeeri's program", "900x700", r'C:\Users\caspe\Desktop\Git test\ME-and-Volerii\BEEAP20_week2and3exc-master\images\favicon.ico',"black")
-def openfile():
-    name = fd.askopenfilename(title="select a CSV file",filetypes=(("csv files", "*.csv"),)) #only csv accepted
-    label_file["text"]=name
-    return None    
-
-def Load_excel_data():
-    file_path = label_file["text"]
-    try:
-        excel_filename = r"{}".format(file_path)
-        df = pd.read_csv(excel_filename)
-       
-    except ValueError:
-        tk.messagebox.showerror("Information", "The file you have chosen is invalid")
-        return None
-    except FileNotFoundError:
-        tk.messagebox.showerror("Information", f"No such file as {file_path}")
-        return None
-
-def combo():
-    df=df.dropna()
-    df.head()
-    drop=df.loc[df['COMMUNITY AREA NAME'] == window.combo.get()]
-    print(drop)
-
-screen()
-#file_path = ttk.Label(root, text="No file selected").place(x=30, y=150)
->>>>>>> Stashed changes
 
 
 
