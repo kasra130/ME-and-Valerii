9367ad608051ae2f4ee36873adfbcf287e5ad066
russhacker <valerii.matveev@student.hamk.fi>
2021-04-17 13:07:19 +0300

    Update backend.py

--- BEEAP20_week2and3exc-master/backend.py
+++ BEEAP20_week2and3exc-master/backend.py
@@ -1,16 +1,16 @@
-class Graph(self):
+class Graph(self):                                     
         self.graph1location1= tk.Frame(frame2)     #frame
-        self.graph1location1.place(x=200,y=500)     #location of graph
-        fig = plt.figure(figsize=(10, 5), dpi=100,)  #creating an emty plot
-        self.ax = fig.add_subplot(111)            #defined as ax0
-        self.chart = FigureCanvasTkAgg(fig, self.graph1location1)  #presented as chart
-        self.chart.get_tk_widget().pack()                          #pack() command for matching with tkkinter        
+        self.graph1location1.place(x=10,y=180)     #location of graph
+        fig = plt.figure(figsize=(7, 4), dpi=50,)  #creating an emty plot
+        self.ax0 = fig.add_subplot(111)            #defined as ax0
+        self.chart1 = FigureCanvasTkAgg(fig, self.graph1location1)  #presented as chart
+        self.chart1.get_tk_widget().pack()                          #pack() command for matching tkkinter
+ 
+ 
+ 
+ 
 
-
-
-
-           #finding value for January
+ #finding value for January
         self.ax0.set_title("KWH Jan 2010 bar"); self.ax0.set_xlabel(x); self.ax0.set_ylabel(y)
-        Januarykwh = self.subdf.columns.get_loc['KWH JANUARY 2010']     #find the  column   
-        self.ax0.bar( (self.subdf.iloc[:, range(Januarykwh, (Januarykwh+12)]).mean())   # getting the mean  
-
+        Januarykwh = self.subdf.columns.get_loc('KWH JANUARY 2010')     #find the column   
+        self.ax0.bar(range(1, 13), (self.subdf.iloc[:, range(Januarykwh, (Januarykwh+12))]) #getting the mean
